b7f0c4ff058705fc6aa0e35e08c437cd9bd99eb6
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2025-12-26 11:24:49 +0100

    fix(loader): svg, circle and path on native apps should be imported

--- components/shared/Loader.tsx
+++ components/shared/Loader.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Animated, Easing } from "react-native";
+import { Circle, Path, Svg } from "react-native-svg";
 
 import Box from "../Box";
 
@@ -10,7 +11,7 @@ interface LoaderVariantProps {
 
 type LoaderProps = LoaderVariantProps & React.SVGProps<SVGSVGElement>;
 
-export const Loader = ({ width, height, ...props }: Omit<LoaderProps, "color">) => {
+export const Loader = ({ width, height }: Omit<LoaderProps, "color">) => {
   const spinValue = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
@@ -42,27 +43,24 @@ export const Loader = ({ width, height, ...props }: Omit<LoaderProps, "color">) 
       height={height}
     >
       <Animated.View style={{ transform: [{ rotate: spin }] }}>
-        <svg
+        <Svg
           width={width}
           height={height}
-          xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          {...props}
         >
-          <circle
-            className="opacity-20"
+          <Circle
             cx="12"
             cy="12"
             r="10"
             stroke="#cbcbcb"
             strokeWidth="4"
           />
-          <path
+          <Path
             fill="#000000"
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           />
-        </svg>
+        </Svg>
       </Animated.View>
     </Box>
   );
